5f31e182b66916cddf187e0c6d79603772419b98
Ericktsw <ericklopes2023@gmail.com>
2025-07-14 17:39:45 -0300

    Update: Remove sections, fix mobile layout, add hacker background image

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/edi-esdisruptivass">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/hero-demo" element={<HeroDemo />} />
